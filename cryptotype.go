4b7810f7f56fe738156244045aed52f31db8fe69
Hunter Chasens <hunter.chasens18@ncf.edu>
2020-12-11 12:43:39 -0500

    error with handlers

--- cryptotype.go
+++ cryptotype.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rand"
 	"crypto/rsa"
+	"encoding/gob"
 	"fmt"
 	"io"
 	"net"
@@ -42,9 +44,55 @@ func decrypt(rand io.Reader, priv *rsa.PrivateKey, ciphertext []byte) ([]byte, e
 }
 
 func connectionHandler(con net.Conn) { // one of these connection handlers is made for every connection made. It'll be in charge of the handshake
-	fmt.Println("connection receaved")
-	con.Write([]byte("Message received."))
-	fmt.Println(con.RemoteAddr())
+
+	defer func() {
+		fmt.Println("Clossing Connection From " + con.RemoteAddr().String())
+	}()
+
+	fmt.Println("Connection Receaved From " + con.RemoteAddr().String())
+
+	//timeoutDur := 5 * time.Second
+	//bufReader := bufio.NewReader(con)
+	//bufWriter := bufio.NewWriter(con)
+
+	bufWriter := bufio.NewWriter(con)
+	enc := gob.NewEncoder(bufWriter)
+	enc.Encode(privKey.PublicKey)
+	bufWriter.Flush()
+
+	//con.Write([]byte("test stff"))
+
+	for {
+		//con.SetReadDeadline(time.Now().Add(timeoutDur))
+		var cryptBytes []byte
+		//cryptBytes, err := bufReader.ReadBytes('\n')
+
+		//i, err := bufReader.Read(cryptBytes)
+		//if err != nil {
+		//	panic(err)
+		//}
+		i, err := con.Read(cryptBytes)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("len is %d", i)
+
+		//bufWriter.WriteString("test")
+
+		//here we would decrypt
+		fmt.Println("Decrypting Message")
+		fmt.Println(cryptBytes)
+		decryptedBytes, err := decrypt(ranReader, privKey, cryptBytes)
+		//if err != nil {
+		//	panic(err)
+		//}
+		fmt.Printf("%s", decryptedBytes)
+	}
+
+	//enc := gob.NewEncoder@con.
+	//fmt.Println("Connection Receaved From " + con.RemoteAddr().String())
+	//con.Write([]byte(privKey.PublicKey))
 
 }
 
@@ -70,7 +118,21 @@ func send(ip string, message string) {
 		fmt.Println("Connection failed. Is port is use?")
 		panic(err)
 	}
-	fmt.Fprintf(conn, message)
+	//get the public key
+	bufReader := bufio.NewReader(conn)
+	dec := gob.NewDecoder(bufReader)
+
+	var pubKey = rsa.PublicKey{}
+	dec.Decode(&pubKey)
+
+	fmt.Println("Public Key Receaved")
+	cryptText, err := encrypt(ranReader, &pubKey, []byte(message))
+	if err != nil {
+		panic(err)
+	}
+
+	conn.Write(cryptText)
+	//fmt.Fprintf(conn, message)
 	//status, err := bufio.NewReader(conn).ReadString('\n')
 }
 
@@ -82,11 +144,46 @@ func main() {
 	ranReader = rand.Reader
 
 	genPrivKey(ranReader)
-
 	fmt.Println("Generated Key Pair")
 
-	go startServer()
+	var startServerBool string
+	fmt.Printf("Start Server? (Yes/No): ")
+	fmt.Scanln(&startServerBool)
+	if startServerBool == "Yes" {
+		go startServer()
+	}
 
-	fmt.Scanln()
+	//send("localhost", "this is a test :)")
+	//fmt.Scanln()
 
+	// Sending Portion
+
+	conn, err := net.Dial("tcp", targetIP+":"+strconv.Itoa(port)) // dials the server over tcp with the IP:Port 	Itoa converts ints to strings
+	if err != nil {
+		fmt.Println("Connection failed. Is port is use?")
+		panic(err)
+	}
+
+	//get the public key
+	bufReader := bufio.NewReader(conn)
+	dec := gob.NewDecoder(bufReader)
+
+	var pubKey = rsa.PublicKey{}
+	dec.Decode(&pubKey)
+
+	fmt.Println("Public Key Receaved")
+
+	var message string
+
+	for {
+		fmt.Scanln(&message)
+		cryptText, err := encrypt(ranReader, &pubKey, []byte(message))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("sending CryptText")
+		fmt.Println(cryptText)
+		conn.Write(cryptText)
+
+	}
 }
